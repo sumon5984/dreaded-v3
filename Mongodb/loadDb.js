70dca8298b52ea4b4f46c28f3ec2846e00dd94c4
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-16 04:51:12 +0300

    Update Mongodb

---
+++ Mongodb/loadDb.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const { mongoURI } = require('./settings');
+
+const connectToDB = async () => {
+  if (!mongoURI || mongoURI.trim() === '') {
+    console.log('No Mongo URL found, please fill the URL.');
+    return;
+  }
+
+  try {
+    if (mongoose.connection.readyState === 0) {
+      await mongoose.connect(mongoURI);
+      console.log('Connected to MongoDB. . .');
+    }
+  } catch (error) {
+    console.log('Failed to connect to MongoDB. Please check the Mongo URI');
+    console.error(error.message);
+  }
+};
+
+module.exports = { connectToDB };
