bf646b74172559accee8aaac7b651a3754729678
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-16 05:00:39 +0300

    Update Settingsdb.js

--- Mongodb/Settingsdb.js
+++ Mongodb/Settingsdb.js
@@ -1,1 +1,38 @@
- 
+const { connectToDB } = require('./loadDb');
+const Settings = require('./Schemas/settingsSchema');
+const GroupSettings = require('./Schemas/groupSettingsSchema');
+
+const getSettings = async () => {
+  try {
+    await connectToDB();
+    const settings = await Settings.findOne();
+    return settings;
+  } catch (error) {
+    console.error('Error fetching settings:', error.message);
+  }
+};
+
+const getGroupSettings = async (jid) => {
+  try {
+    await connectToDB();
+    let groupSettings = await GroupSettings.findOne({ jid });
+
+    if (!groupSettings) {
+      console.log(`Group settings not found for ${jid}, creating new settings.`);
+      groupSettings = new GroupSettings({ jid });
+      await groupSettings.save();
+      console.log(`Created new group settings for ${jid}`);
+    } else {
+      console.log(`Fetched group settings for ${jid}`);
+    }
+
+    return groupSettings;
+  } catch (error) {
+    console.error('Error fetching or creating group settings:', error.message);
+  }
+};
+
+module.exports = {
+  getSettings,
+  getGroupSettings,
+}; 
