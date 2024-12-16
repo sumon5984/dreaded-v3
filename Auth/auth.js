ccf71a0da4b01bc477e96a2cc8be923d43d81d19
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-16 05:55:47 +0300

    Update auth.js

--- Auth/auth.js
+++ Auth/auth.js
@@ -1,1 +1,20 @@
- 
+const fs = require('fs');
+const { session } = require('../config');
+
+async function authenticationn() {
+    try {
+        if (!fs.existsSync("../session/creds.json")) {
+            console.log("📡 connecting...");
+            await fs.writeFileSync("../session/creds.json", atob(session), "utf8");
+        }
+        else if (fs.existsSync("../session/creds.json") && session != "zokk") {
+            await fs.writeFileSync("../session/creds.json", atob(session), "utf8");
+        }
+    }
+    catch (e) {
+        console.log("Session is invalid: " + e);
+        return;
+    }
+}
+
+module.exports = authenticationn; 
