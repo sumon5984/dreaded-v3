9626917395eeaa6532a8eb846757b2b206e6c243
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-16 14:30:05 +0300

    Update antidemote.js

--- Commands/Settings/antidemote.js
+++ Commands/Settings/antidemote.js
@@ -3,7 +3,7 @@ const ownerMiddleware = require('../../Middleware/ownerMiddleware');
 
 module.exports = async (context) => {
     await ownerMiddleware(context, async () => {
-        const { m, args } = context;
+        const { client, m, args } = context;
         const value = args[0]?.toLowerCase();
         const jid = m.chat; 
 
@@ -19,10 +19,24 @@ module.exports = async (context) => {
             await groupSettings.save();
         }
 
+        
+        const Myself = await client.decodeJid(client.user.id);
+
+        
+        let groupMetadata = await client.groupMetadata(m.chat);
+        let userAdmins = groupMetadata.participants.filter(p => p.admin !== null).map(p => p.id);
+
+      
+        const isBotAdmin = userAdmins.includes(Myself);
+
+        if (!isBotAdmin) {
+            return await m.reply('I need admin privileges to manage anti-spam.');
+        }
+
         if (value === 'on') {
             groupSettings.antidemote = true;
             await groupSettings.save();
-            await m.reply(`✅ Antidemote has been turned ON for this group. Bot will now detect denotes and restrict some!`);
+            await m.reply(`✅ Antidemote has been turned ON for this group. Bot will now detect demotes and restrict some!`);
         } else if (value === 'off') {
             groupSettings.antidemote = false;
             await groupSettings.save();
