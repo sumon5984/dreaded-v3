d845d61c6618ba31168f47d640204975182003d3
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-16 04:41:09 +0300

    Update settingsSchema.js

--- Mongodb/Schemas/settingsSchema.js
+++ Mongodb/Schemas/settingsSchema.js
@@ -1,1 +1,64 @@
- 
+const mongoose = require('mongoose');
+
+
+const settingsSchema = new mongoose.Schema({
+  autoread: {
+    type: Boolean,
+    default: true 
+  },
+  autoviewstatus: {
+    type: Boolean,
+    default: true
+  },
+  autolikestatus: {
+    type: Boolean,
+    default: true
+  },
+  autobio: {
+    type: Boolean,
+    default: false
+  },
+  anticall: {
+    type: Boolean,
+    default: false
+  },
+  antionce: {
+    type: Boolean,
+    default: true
+  },
+  presence: {
+    type: String,
+    default: 'online'
+  },
+  prefix: {
+    type: String,
+    default: '.'
+  },
+  author: {
+    type: String,
+    default: 'fortunatus'
+  },
+  packname: {
+    type: String,
+    default: 'dreaded md2 🤖'
+  },
+  dev: {
+    type: String,
+    default: '254114018035'
+  },
+  DevDreaded: {
+    type: [String], 
+    default: ['254114018035']
+  },
+  botname: {
+    type: String,
+    default: 'DREADED'
+  },
+  mode: {
+    type: String,
+    default: 'public'
+  }
+});
+
+
+module.exports = mongoose.model('Settings', settingsSchema); 
