7b8dff77a4cae1da3731deebf9d3dbdf8d46e556
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-16 12:31:16 +0300

    Update Settings

---
+++ Commands/Settings/antidemote.js
@@ -0,0 +1,34 @@
+const { getGroupSettings } = require('../../Mongodb/Settingsdb');
+const ownerMiddleware = require('../../Middleware/ownerMiddleware');
+
+module.exports = async (context) => {
+    await ownerMiddleware(context, async () => {
+        const { m, args } = context;
+        const value = args[0]?.toLowerCase();
+        const jid = m.chat; 
+
+        if (!jid.endsWith('@g.us')) {
+            return await m.reply('❌ This command can only be used in groups.');
+        }
+
+        let groupSettings = await getGroupSettings(jid);
+
+        if (!groupSettings) {
+            const GroupSettings = require('../../Mongodb/Schemas/groupSettingsSchema');
+            groupSettings = new GroupSettings({ jid });
+            await groupSettings.save();
+        }
+
+        if (value === 'on') {
+            groupSettings.antidemote = true;
+            await groupSettings.save();
+            await m.reply(`✅ Antidemote has been turned ON for this group. Bot will now restrict demotes!`);
+        } else if (value === 'off') {
+            groupSettings.antidemote = false;
+            await groupSettings.save();
+            await m.reply(`❌ Antidemote has been turned OFF for this group.`);
+        } else {
+            await m.reply(`📄 Current antidemote setting for this group: ${groupSettings.antidemote ? 'ON' : 'OFF'}\n\n Use "antidemote on" or "antidemote off".`);
+        }
+    });
+};
