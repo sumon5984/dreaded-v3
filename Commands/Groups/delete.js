e54e83f5f85cc21f87602613f82e97456b4588f3
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-16 11:16:08 +0300

    Update Groups

---
+++ Commands/Groups/delete.js
@@ -0,0 +1,18 @@
+const middleware = require('../../Middleware/adminMiddleware');
+
+module.exports = async (context) => {
+    await middleware(context, async () => {
+        const { client, m } = context;
+
+        if (!m.quoted) return m.reply(`Did you quote a message?`);
+
+        client.sendMessage(m.chat, { 
+            delete: { 
+                remoteJid: m.chat, 
+                fromMe: false, 
+                id: m.quoted.id, 
+                participant: m.quoted.sender 
+            } 
+        });
+    });
+};
