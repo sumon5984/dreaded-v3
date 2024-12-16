d8f22e7aa8a9889e194f03660c99556f99d6d924
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-16 23:01:43 +0300

    Update gcpresence.js

--- Functions/gcpresence.js
+++ Functions/gcpresence.js
@@ -1,1 +1,18 @@
- 
+const { getGroupSettings, getSettings } = require('../Mongodb/Settingsdb');
+
+module.exports = async (client, m) => {
+  const userId = m.sender;
+  const groupId = m.chat;
+  
+  
+    
+  if (!groupId.endsWith("@g.us")) return;
+const groupSettings = await getGroupSettings(groupId);
+  if (groupSettings.gcpresence) {
+  
+  let dreadrecordin = ['recording', 'composing'];
+        let dreadrecordinfinal = dreadrecordin[Math.floor(Math.random() * dreadrecordin.length)];
+        await client.sendPresenceUpdate(dreadrecordinfinal, m.chat);
+        
+        
+};  
