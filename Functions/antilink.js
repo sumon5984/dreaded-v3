73131a18ab141b03ba2308a291eb5df0852b35f6
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-16 21:23:20 +0300

    Update antilink.js

--- Functions/antilink.js
+++ Functions/antilink.js
@@ -1,1 +1,68 @@
- 
+const { getGroupSettings } = require('../Mongodb/Settingsdb');
+
+module.exports = async (client, m) => {
+  const userId = m.sender;
+  const groupId = m.chat;
+
+  if (!groupId.endsWith("@g.us")) return;
+
+  const groupMetadata = await client.groupMetadata(groupId);
+  const groupAdmins = groupMetadata.participants
+    .filter((participant) => participant.admin)
+    .map((participant) => participant.id);
+
+  const botId = client.decodeJid(client.user.id);
+
+  if (!groupAdmins.includes(botId)) {
+    console.log(`Bot is not an admin in group ${groupId}. No action taken.`);
+    return;
+  }
+
+  if (groupAdmins.includes(userId)) {
+    console.log(`User ${userId} is an admin in group ${groupId}. No action taken.`);
+    return;
+  }
+
+  const groupSettings = await getGroupSettings(groupId);
+  if (!groupSettings.antilink) {
+    console.log(`Antilink is not enabled for group ${groupId}. No action taken.`);
+    return;
+  }
+
+  var body =
+    m.mtype === "conversation"
+      ? m.message.conversation
+      : m.mtype === "imageMessage"
+      ? m.message.imageMessage.caption
+      : m.mtype === "extendedTextMessage"
+      ? m.message.extendedTextMessage.text
+      : "";
+
+  if (body && (body.includes('http://') || body.includes('https://'))) {
+    console.log(`Detected a link in a message from ${userId}.`);
+
+    await client.sendMessage(
+      groupId,
+      {
+        text: `@${userId.split("@")[0]}, sharing links is not allowed in this group!`,
+        contextInfo: {
+          mentionedJid: [userId],
+        },
+      },
+      { quoted: m }
+    );
+
+    await client.groupParticipantsUpdate(groupId, [userId], "remove");
+
+    await client.sendMessage(
+      groupId,
+      {
+        text: `@${userId.split("@")[0]} has been removed for sending a link.\nAntilink is active!`,
+        contextInfo: {
+          mentionedJid: [userId],
+        },
+      },
+      { quoted: m }
+    );
+  }
+}; 
