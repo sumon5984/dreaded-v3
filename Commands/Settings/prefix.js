6469470493be758d252262f7ada11d7dfdd543e8
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-16 15:59:12 +0300

    Update prefix.js

--- Commands/Settings/prefix.js
+++ Commands/Settings/prefix.js
@@ -9,16 +9,23 @@ module.exports = async (context) => {
         let settings = await getSettings();
 
         if (!settings) {
+            const Settings = require('../../Mongodb/Schemas/settingsSchema');
             settings = new Settings();
             await settings.save();
         }
 
         if (newPrefix) {
             if (newPrefix === 'null') {
+                if (!settings.prefix) {
+                    return await m.reply(`✅ The bot was already prefixless.`);
+                }
                 settings.prefix = "";
                 await settings.save();
                 await m.reply(`✅ The bot is now prefixless.`);
             } else {
+                if (settings.prefix === newPrefix) {
+                    return await m.reply(`✅ The prefix was already set to: ${newPrefix}`);
+                }
                 settings.prefix = newPrefix;
                 await settings.save();
                 await m.reply(`✅ Prefix has been updated to: ${newPrefix}`);
