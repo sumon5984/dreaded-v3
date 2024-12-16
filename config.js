38aee665b4df066e1e51491a5b107710e26ad36c
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-16 05:26:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,11 @@
- 
+const session = process.env.SESSION || '';
+const mongoURI = process.env.MONGO_URI || '';
+const mycode = process.env.CODE || '254';
+const botname = process.env.BOTNAME || 'DREADED';
+
+module.exports = {
+  session,
+  mongoURI,
+  mycode,
+  botname
+}; 
