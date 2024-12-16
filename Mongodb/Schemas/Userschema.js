9716f409a2827578c1c75c96ef10c6f263feaa02
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-16 04:38:38 +0300

    Update Userschema.js

--- Mongodb/Schemas/Userschema.js
+++ Mongodb/Schemas/Userschema.js
@@ -1,1 +1,21 @@
- 
+const mongoose = require('mongoose');
+
+
+const newUserSchema = new mongoose.Schema({
+  jid: {
+    type: String,
+    required: true,
+    unique: true, 
+  },
+  banned: {
+    type: Boolean,
+    default: false, 
+  },
+  banReason: {
+    type: String,
+    default: null, 
+  },
+});
+
+
+module.exports = mongoose.model('NewUser', newUserSchema); 
