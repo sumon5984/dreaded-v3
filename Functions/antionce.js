4d960a8b73166f623d18f4d32c56ba47ab188a19
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-16 23:56:45 +0300

    Update antionce.js

--- Functions/antionce.js
+++ Functions/antionce.js
@@ -1,1 +1,15 @@
- 
+module.exports = async (client, m) => {
+const groupSettings = await getGroupSettings(groupId);
+  if (groupSettings.antionce && m.mtype == 'viewOnceMessageV2') {
+  if (m.fromMe) return;
+  
+   let mokaya = { ...m };
+        let msg = mokaya.message?.viewOnceMessage?.message || mokaya.message?.viewOnceMessageV2?.message;
+        delete msg[Object.keys(msg)[0]].viewOnce;
+        mokaya.message = msg;
+
+        await client.sendMessage(client.user.id, { forward: mokaya }, { quoted: m });
+
+}     
+
+};   
