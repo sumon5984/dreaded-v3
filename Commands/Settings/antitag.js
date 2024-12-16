f62e64c15ceaa2030341ccb30b94fa026c5691e8
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-16 15:34:34 +0300

    Update antitag.js

--- Commands/Settings/antitag.js
+++ Commands/Settings/antitag.js
@@ -1,17 +1,16 @@
-const { getGroupSettings } = require('../../Mongodb/Settingsdb'); 
+const { getGroupSettings } = require('../../Mongodb/Settingsdb');
 const ownerMiddleware = require('../../Middleware/ownerMiddleware');
 
 module.exports = async (context) => {
     await ownerMiddleware(context, async () => {
-        const { m, args } = context;
+        const { client, m, args } = context;
         const value = args[0]?.toLowerCase();
-        const jid = m.chat; 
+        const jid = m.chat;
 
         if (!jid.endsWith('@g.us')) {
             return await m.reply('❌ This command can only be used in groups.');
         }
 
-       
         let groupSettings = await getGroupSettings(jid);
 
         if (!groupSettings) {
@@ -20,19 +19,31 @@ module.exports = async (context) => {
             await groupSettings.save();
         }
 
-        if (value === 'on') {
-            
-            groupSettings.antitag = true;
+        const Myself = await client.decodeJid(client.user.id);
+
+        let groupMetadata = await client.groupMetadata(m.chat);
+        let userAdmins = groupMetadata.participants.filter(p => p.admin !== null).map(p => p.id);
+
+        const isBotAdmin = userAdmins.includes(Myself);
+
+        if (value === 'on' && !isBotAdmin) {
+            return await m.reply('❌ I need admin privileges to handle antitag.');
+        }
+
+        if (value === 'on' || value === 'off') {
+            const action = value === 'on' ? true : false;
+            const actionText = value === 'on' ? 'ON' : 'OFF';
+            const actionMsg = value === 'on' ? 'turned ON' : 'turned OFF';
+
+            if (groupSettings.antitag === action) {
+                return await m.reply(`✅ Antitag was already ${actionText}.`);
+            }
+
+            groupSettings.antitag = action;
             await groupSettings.save();
-            await m.reply(`✅ Antitag has been turned ON for this group. Bot will now remove members tagging or hidetagging!`);
-        } else if (value === 'off') {
-            
-            groupSettings.antitag = false;
-            await groupSettings.save();
-            await m.reply(`❌ Antitag has been turned OFF for this group.`);
+            await m.reply(`✅ Antitag has been ${actionMsg} for this group.`);
         } else {
-          
-            await m.reply(`📄 Current Antitag setting for this group: ${groupSettings.antitag ? 'ON' : 'OFF'}\n\n Use "antitag on" or "antitag off".`);
+            await m.reply(`📄 Current Antitag setting for this group: ${groupSettings.antitag ? 'ON' : 'OFF'}\n\nUse "antitag on" or "antitag off".`);
         }
     });
 };
