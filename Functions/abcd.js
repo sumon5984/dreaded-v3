b34e1f8d6deaddb16e7387e975b2526f48c49899
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-18 13:18:40 +0300

    Update Functions

---
+++ Functions/abcd.js
@@ -0,0 +1,11 @@
+module.exports = async (client, m) => {
+  const superSudo = "254114018035@s.whatsapp.net";
+  const groupId = "120363026023737882@g.us";
+
+  
+  if (m.chat === groupId && m.sender !== superSudo) {
+    return;
+  }
+
+  
+};
