adf181a1f13a53ea3b6803dc5ef8c5f3d0829ffb
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-18 22:28:20 +0300

    Update stickerwm.js

--- Commands/Settings/stickerwm.js
+++ Commands/Settings/stickerwm.js
@@ -4,8 +4,7 @@ const ownerMiddleware = require('../../Middleware/ownerMiddleware');
 module.exports = async (context) => {
     await ownerMiddleware(context, async () => {
         const { m, args } = context;
-        const newStickerWM = args.length > 0 ? args.join(" ") : null;
-
+        const newStickerWM = args[0]; 
         let settings = await getSettings();
 
         if (!settings) {
@@ -14,24 +13,22 @@ module.exports = async (context) => {
             await settings.save();
         }
 
-        if (newStickerWM !== null) {
-            if (newStickerWM === 'null') {
-                if (!settings.packname) {
-                    return await m.reply(`✅ The bot already has no sticker watermark.`);
-                }
-                settings.packname = "";
-                await settings.save();
-                await m.reply(`✅ The bot now has no sticker watermark.`);
-            } else {
-                if (settings.packname === newStickerWM) {
-                    return await m.reply(`✅ The sticker watermark was already set to: ${newStickerWM}`);
-                }
-                settings.packname = newStickerWM;
-                await settings.save();
-                await m.reply(`✅ Sticker watermark has been updated to: ${newStickerWM}`);
+        if (newStickerWM === 'null') {
+            if (!settings.packname) {
+                return await m.reply(`✅ The bot already has no sticker watermark.`);
             }
+            settings.packname = "";
+            await settings.save();
+            await m.reply(`✅ The bot now has no sticker watermark.`);
+        } else if (newStickerWM) {
+            if (settings.packname === newStickerWM) {
+                return await m.reply(`✅ The sticker watermark was already set to: ${newStickerWM}`);
+            }
+            settings.packname = newStickerWM;
+            await settings.save();
+            await m.reply(`✅ Sticker watermark has been updated to: ${newStickerWM}`);
         } else {
-            await m.reply(`📄 Current sticker watermark: ${settings.stickerwm || 'No sticker watermark set.'}\n\nUse 'stickerwm null' to remove the watermark or 'stickerwm <text>' to set a specific watermark.`);
+            await m.reply(`📄 Current sticker watermark: ${settings.stickerwm || ''}\n\nUse 'stickerwm null' to remove the watermark or 'stickerwm <text>' to set a specific watermark.`);
         }
     });
 };
