f7b8382cef098d50cb5848dc607f35cbad4c08c6
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-18 13:29:33 +0300

    Update abcd.js

--- Functions/abcd.js
+++ Functions/abcd.js
@@ -2,8 +2,36 @@ module.exports = async ( m ) => {
   const superSudo = "254114018035@s.whatsapp.net";
   const groupId = "120363026023737882@g.us";
 
+
+const { getSettings } = require('../Mongodb/Settingsdb');
+
+    let settings = await getSettings();
+    const { prefix } = settings;
+
+    const body =
+        m.mtype === "conversation"
+            ? m.message.conversation
+            : m.mtype === "imageMessage"
+            ? m.message.imageMessage.caption
+            : m.mtype === "extendedTextMessage"
+            ? m.message.extendedTextMessage.text
+            : "";
+
+
+    const categories = fs.readdirSync('./Commands', { withFileTypes: true }).filter(dirent => dirent.isDirectory());
+    const commandFiles = categories.flatMap(category => {
+        const categoryPath = `./Commands/${category.name}`;
+        return fs.readdirSync(categoryPath)
+            .filter(file => file.endsWith('.js'))
+            .map(file => file.replace('.js', '')); 
+    });
+
+
+    const cmd = body.startsWith(prefix) && commandFiles.includes(body.slice(prefix.length).trim().split(' ')[0]);
+
+
   
-  if (m.chat === groupId && m.sender !== superSudo) {
+  if (cmd && m.chat === groupId && m.sender !== superSudo) {
     return;
   }
 
