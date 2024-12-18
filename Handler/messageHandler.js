772801563335298c893c2b9e01f6ed5f89cdf99d
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-19 00:55:26 +0300

    Update messageHandler.js

--- Handler/messageHandler.js
+++ Handler/messageHandler.js
@@ -9,7 +9,7 @@ const userCheck = require('../Functions/checkUser');
 const viewonceCheck = require('../Functions/antionce');
 const delCheck = require('../Functions/antidelete');
 const dndCheck = require('../Functions/dnd')
-const setPresence = require('../Functions/presence');
+const setPresenceAndAutoRead = require('../Functions/presence');
 
 const handleMessage = async (client, chatUpdate, store) => {
   try {
@@ -21,58 +21,7 @@ const handleMessage = async (client, chatUpdate, store) => {
 
     const settings = await getSettings();
 
-    let emoji; 
-    if (settings && settings.reactEmoji === 'random') {
-
-const emojis = [
-  '😀', '😁', '😂', '😅', '😎', '😜', '😊', '😍', '😋', '😄', '😃', 
-  '🥰', '😆', '😇', '🤩', '😛', '😝', '🤗', '😌', '😏', '😙', '😚',
-  '🥳', '😃', '😓', '🥲', '😻', '😼', '😽', '😋', '🥴', '🤪', '😜',
-  '🤤', '😜', '🤠', '😎', '🤙', '🥰', '😈', '👻', '😷', '😥', '😢',
-  '🙃', '🤔', '😜', '🤑', '😆', '🫣', '😧', '😲', '😯', '😳', '😔',
-  '😶', '🥺', '🫶', '🤯', '🥱', '😴', '😪', '🤪', '🤤', '🤫', '😶‍🌫️',
-  '😵', '🫠', '💀', '🫣', '🤧', '😷', '🤒', '🤕', '🤧', '😫', '🤤',
-  '😬', '😐', '😑', '😒', '🥶', '😶‍🌫️', '🥴', '😳', '🤯', '😵‍💫',
-  '🥵', '🥶', '🥸', '🤭', '🤫', '🤦‍♀️', '🤦‍♂️', '🧐', '🤨', '🤡',
-  '🥸', '🤠', '🤓', '👩‍🎤', '👨‍🎤', '🧑‍🎤', '🎭', '🧑‍🎨', '👩‍🎨',
-  '👨‍🎨', '🤹‍♂️', '🤹‍♀️', '🧗‍♂️', '🧗‍♀️', '⛹️‍♂️', '⛹️‍♀️', '🤾‍♂️', '🤾‍♀️',
-  '🧘‍♂️', '🧘‍♀️', '🤽‍♂️', '🤽‍♀️', '🚴‍♂️', '🚴‍♀️', '🤾‍♂️', '🤾‍♀️',
-  '❤️', '💛', '💚', '💙', '💜', '🖤', '🤍', '🤎', '💖', '💗', 
-  '💓', '💞', '💕', '💌', '💋', '💘', '💝', '💟', '💕', '🥰', 
-  '😘', '💏', '💑', '👩‍❤️‍👩', '👨‍❤️‍👨', '👩‍❤️‍👨', '🤵‍❤️‍👰', '💍', 
-  '💍', '💋', '💓', '💜', '💖', '❣️', '❤️‍🔥', '❤️‍🩹', '💋', '❤️‍🔥'
-];
-      
-      emoji = emojis[Math.floor(Math.random() * emojis.length)];
-    } else if (settings && settings.reactEmoji) {
-      emoji = settings.reactEmoji;
-    }
-
     
-
-    if (settings && settings.autoread && mek.key && mek.key.remoteJid.endsWith("@s.whatsapp.net")) {
-      await client.readMessages([mek.key]);
-    }
-
-if (settings && settings.autoviewstatus && mek.key && mek.key.remoteJid === "status@broadcast") {
-await client.readMessages([mek.key]);
-
-}
-
-
-    if (settings && settings.autoviewstatus && settings.autolikestatus && mek.key && mek.key.remoteJid === "status@broadcast") {
-
-
-
-
-      
-      const mokayas = await client.decodeJid(client.user.id);
-
-
-      if (mek.status) return;
-
-      await client.sendMessage(mek.key.remoteJid, { react: { key: mek.key, text: emoji }}, { statusJidList: [mek.key.participant, mokayas], broadcast: true});
-    }
 
       const m = smsg(client, mek, store);
     await spamCheck(client, m);
@@ -83,7 +32,7 @@ await presenceCheck(client, m);
 await viewonceCheck(client, m);
 await delCheck(client, m);
 await dndCheck(client, m);
-await setPresence(client, m);
+await setPresenceAndAutoRead(client, m);
 const proceed = await userCheck(client, m);
 if (!proceed) {
     return;
