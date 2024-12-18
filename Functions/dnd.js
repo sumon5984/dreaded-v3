9679b7e4e6d564179d763016ad027b45c065f515
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-18 09:54:01 +0300

    Update Functions

---
+++ Functions/dnd.js
@@ -0,0 +1,47 @@
+const { getGroupSettings, getSettings } = require('../Mongodb/Settingsdb');
+const { getUser, createUser } = require('../Mongodb/Userdb'); 
+
+module.exports = async (client, m) => {
+const { default: Gemini } = await import('gemini-ai');
+
+  const jid = m.sender;
+    const chatId = m.chat;
+    const userInput = m.text;
+    const name = `dreaded digital assistant`;
+    const botId = client.decodeJid(client.user.id);
+    
+      if (jid.includes(botId)) return;
+      
+        if (!chatId.endsWith("@s.whatsapp.net")) return;
+        
+          let settings = await getSettings();
+          if (!settings.dnd) return;
+          
+          if (userInput) {
+          const prompt = `You are a WhatsApp digital assistant. You will be responsible for handling this whatsapp account while i am offline. Your name will be ${name}. You will respond to messages politely saying i am currently offline and you are in charge. You will ask for their names if they have not told you yet so that in the messages you can refer to 
+          the sender by their name. Your name will be Respond directly to user queries without prefixes like "Assistant's response".`;
+          
+          let user = await getUser(jid);
+    if (!user) {
+      user = await createUser(jid);
+    }
+    
+    user.messages.push({ sender: 'user', content: userInput });
+    await user.save();
+    
+    const history = user.messages.map(msg => `${msg.sender}: ${msg.content}`).join('\n');
+    const instruction = `${prompt}\nChat history:\n${history}\nUser's input: ${userInput}`;
+    
+    const gemini = new Gemini('AIzaSyC3sNClbdraGrS2ubb5PTdnm_RbUANtdzc');
+    const chat = gemini.createChat();
+    let res = await chat.ask(instruction);
+    
+    res = res.replace(/^Assistant['’]?s? response:\s*/i, '');
+    
+    user.messages.push({ sender: 'assistant', content: res });
+    await user.save();
+    
+    await m.reply(res);
+    
+    }
+    
