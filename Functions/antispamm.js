33aaecbe93c0e00b356c5c6663c9c9ed7761e754
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-18 21:09:43 +0300

    Update antispamm.js

--- Functions/antispamm.js
+++ Functions/antispamm.js
@@ -23,12 +23,12 @@ module.exports = async (client, m) => {
   const botId = client.decodeJid(client.user.id);
 
   if (!groupAdmins.includes(botId)) {
-    console.log(`Bot is not an admin in group ${groupId}. No action taken.`);
+    
     return;
   }
 
   if (groupAdmins.includes(userId)) {
-    console.log(`User ${userId} is an admin in group ${groupId}. No action taken.`);
+    
     return;
   }
 
@@ -39,19 +39,19 @@ module.exports = async (client, m) => {
   const currentDevs = settings.dev.split(',').map((num) => num.trim());
 
   if (currentDevs.includes(checkdev)) {
-    console.log(`User ${userId} is a sudo user. No action taken.`);
+    
     return;
   }
 
   const groupSettings = await getGroupSettings(groupId);
   if (!groupSettings.antispam) {
-    console.log(`Anti-spam is not enabled for group ${groupId}. No action taken.`);
+    
     return;
   }
 
   if (isSpamming(userId)) {
     if (!hasBeenWarned(userId)) {
-      console.log(`User ${userId} is spamming.`);
+      
 
       await client.sendMessage(
         groupId,
@@ -82,20 +82,18 @@ module.exports = async (client, m) => {
         let user = await getUser(userId);  
 
         if (!user) {
-          console.log(`User ${userId} not found. Creating user and banning them for spamming.`);
+          
           user = await createUser(userId); 
           user.banned = true;
           user.banReason = 'spamming';
         } else if (!user.banned) {
-          console.log(`User ${userId} found. Updating banned status due to spamming.`);
+          
           user.banned = true;
           user.banReason = 'spamming';
-        } else {
-          console.log(`User ${userId} is already banned. Reason: ${user.banReason || 'No reason provided'}`);
-        }
+        } 
 
         await user.save();
-        console.log(`User ${userId} banned for spamming.`);
+        
       } catch (error) {
         console.error(`Error handling ban for ${userId}:`, error.message);
       }
