98c8b1f95cf1f20c41b4f193874cc4857a569327
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-19 00:51:00 +0300

    Update presence.js

--- Functions/presence.js
+++ Functions/presence.js
@@ -1,7 +1,8 @@
 const { getSettings } = require("../Mongodb/Settingsdb");
 
-const setPresence = async (client, m) => {
+const setPresenceAndAutoRead = async (client, m) => {
   const settings = await getSettings();
+
   
   if (m.key && m.key.remoteJid.endsWith('@s.whatsapp.net')) {
     const Chat = m.key.remoteJid;
@@ -15,6 +16,11 @@ const setPresence = async (client, m) => {
       await client.sendPresenceUpdate("unavailable", Chat);
     }
   }
+
+  
+  if (settings && settings.autoread && m.key && m.key.remoteJid.endsWith("@s.whatsapp.net")) {
+    await client.readMessages([m.key]);
+  }
 };
 
-module.exports = setPresence;
+module.exports = setPresenceAndAutoRead;
