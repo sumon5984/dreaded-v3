3a5e7529a8ad883a3c75d16adce818937ab62b8b
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-18 18:00:57 +0300

    Update gpt.js

--- Commands/AI/gpt.js
+++ Commands/AI/gpt.js
@@ -3,17 +3,19 @@ const { getUser, createUser } = require('../../Mongodb/Userdb');
 module.exports = async (context) => {
   const { client, m, text } = context;
 
-  if (!text) return m.reply('Text ?');
+  if (!text) return m.reply('Provide text to the AI. If my responses become redundant use gpt -reset to clear your conversation history.');
   const { default: Gemini } = await import('gemini-ai');
 
+if (!process.env.GEMINI_API_KEY) return m.reply('I need a Gemini API key to handle this request. Please insert the key in the GEMINI_API_KEY environment variable.');
+
   const master = process.env.MASTER_NAME;
-  const prompt = `You are a WhatsApp digital assistant. Respond directly to user queries without prefixes like "Assistant's response".`;
+  const prompt = `You are a WhatsApp digital assistant called DREADED. Respond directly to user queries without prefixes like "Assistant's response".`;
 
   const jid = m.sender;
   const userInput = text;
 
   try {
-    if (userInput.toLowerCase() === 'reset') {
+    if (userInput.toLowerCase() === '-reset') {
       let user = await getUser(jid);
       if (user) {
         user.logs = [];
