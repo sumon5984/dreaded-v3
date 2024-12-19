0d6ac3a16c986e363a15311523b372a1dd969f59
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-19 16:45:44 +0300

    Update play.js

--- Commands/Media/play.js
+++ Commands/Media/play.js
@@ -2,7 +2,6 @@ const axios = require("axios");
 
 module.exports = async (context) => {
     const { client, m, text, fetchJson } = context;
-
     const yts = require("yt-search");
 
     try {
@@ -14,21 +13,28 @@ module.exports = async (context) => {
 
         await m.reply(`_Downloading ${name}_`);
 
-       
+        const proxy = {
+            host: "13.36.104.85", // Replace with a valid proxy host
+            port: 80, // Replace with the proxy port
+            auth: {
+                username: "", // Replace with proxy username (if required)
+                password: "", // Replace with proxy password (if required)
+            },
+        };
+
         const response = await axios.get(audioLink, {
             responseType: "arraybuffer",
             headers: {
                 "User-Agent": "Mozilla/5.0",
             },
+            proxy,
         });
 
-      
         await client.sendMessage(m.chat, {
             document: Buffer.from(response.data),
             mimetype: "audio/mpeg",
             fileName: name,
         }, { quoted: m });
-
     } catch (error) {
         m.reply("Download failed\n" + error.message);
     }
