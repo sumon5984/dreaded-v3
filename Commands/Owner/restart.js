76be1a2196fdb1ab9b5a64a29c33affb7845858d
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2024-12-19 16:26:02 +0300

    Update restart.js

--- Commands/Owner/restart.js
+++ Commands/Owner/restart.js
@@ -4,23 +4,13 @@ module.exports = async (context) => {
     await ownerMiddleware(context, async () => {
         const { client, m, text, Owner } = context;
 
-const { exec } = require('child_process');
-
-exec('node index.js', (err, stdout, stderr) => {
-  if (err) {
-
-m.reply('Error occured\n' + err)
-    console.error(err);
-    return;
-  } else { 
-
-m.reply("Restarting. . .")
-console.log(stdout);
-
-}
 
   
-});
+ await m.reply("Restarting. . .");
+  
+  process.exit()  
+
+
 
 })
 
