d1f654b683e8dc6d40dac5cdd980d6e2f21f263d
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-01-05 02:44:05 +0300

    Update Media

---
+++ Commands/Media/ytmp3.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+
+module.exports = async (context) => {
+    const { client, m, text, fetchJson } = context;
+
+    let urls = text.match(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch\?v=|v\/|embed\/|shorts\/|playlist\?list=)?)([a-zA-Z0-9_-]{11})/gi);
+        if (!urls) return m.reply('provide a valid YouTube link, eh ?');
+
+    try {
+                
+
+        let data = await fetchJson(`https://api.dreaded.site/api/ytdl/audio?url=${text}`);
+        let name = data.title;
+        let audio = data.audioUrl;
+
+        await m.reply(`_Downloading ${name}_`);
+
+
+        await client.sendMessage(m.chat, {
+ document: {url: audio },
+mimetype: "audio/mpeg",
+ fileName: name }, { quoted: m });
+
+await client.sendMessage(m.chat, {
+ document: {url: audio},
+mimetype: "audio/mpeg",
+ fileName: name }, { quoted: m });
+
+
+
+    } catch (error) {
+        m.reply("Download failed\n" + error.message);
+    }
+};
