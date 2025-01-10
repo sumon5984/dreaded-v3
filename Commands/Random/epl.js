bd27a07f0f9bf57f38ac43a14c30345724ce9750
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-01-10 15:44:22 +0300

    Update epl.js

--- Commands/Random/epl.js
+++ Commands/Random/epl.js
@@ -3,12 +3,14 @@ module.exports = async (context) => {
     const { client, m, text, fetchJson } = context;
 
     try {
-        
+
         const data = await fetchJson('https://api.dreaded.site/api/standings/PL');
-        
+
         const standings = data.data;
 
-        await m.reply(standings);
+        const message = `EPL TABLE STANDINGS\n\n${standings}`;
+
+        await m.reply(message);
 
     } catch (error) {
         m.reply('Something went wrong. Unable to fetch EPL standings.');
