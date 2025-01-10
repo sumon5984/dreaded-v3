4518a31b5223b221c042a74dfa4e3777965f3083
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-01-10 11:15:06 +0300

    Update video.js

--- Commands/Media/video.js
+++ Commands/Media/video.js
@@ -7,34 +7,42 @@ module.exports = async (context) => {
 
     try {
         if (!text) return m.reply("What video do you want to download?");
-            const {
-                videos
-            } = await yts(text);
-            if (!videos || videos.length <= 0) {
-                m.reply(`No videos found!`)
-                return;
-            }
-            let urlYt = videos[0].url
+        
+        const { videos } = await yts(text);
+        if (!videos || videos.length <= 0) {
+            m.reply(`No videos found!`);
+            return;
+        }
 
-        let data = await fetchJson(`https://api.dreaded.site/api/ytdl/video?url=${urlYt}`);
+        let urlYt = videos[0].url;
+
+      
+        let data = await fetchJson(`https://api.dreaded.site/api/ytdl2/video?url=${urlYt}`);
+        if (!data.success) {
+            m.reply("Failed to fetch video download URL.");
+            return;
+        }
+
         let name = data.title;
-        let video = data.videoUrl;
+        let videoUrl = data.downloadUrl;
 
         await m.reply(`_Downloading ${name}_`);
 
-
+       
         await client.sendMessage(m.chat, {
- video: {url: video},
-mimetype: "video/mp4", caption: name,
- fileName: name }, { quoted: m });
+            video: { url: videoUrl },
+            mimetype: "video/mp4",
+            caption: name,
+            fileName: name
+        }, { quoted: m });
 
-
-await client.sendMessage(m.chat, {
- document: {url: video},
-mimetype: "video/mp4", caption: name,
- fileName: name }, { quoted: m });
-
-
+        
+        await client.sendMessage(m.chat, {
+            document: { url: videoUrl },
+            mimetype: "video/mp4",
+            caption: name,
+            fileName: name
+        }, { quoted: m });
 
     } catch (error) {
         m.reply("Download failed\n" + error.message);
