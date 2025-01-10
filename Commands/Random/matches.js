82b5c1aa52b3f536861d4d53aff2595edffb04ea
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-01-10 17:09:28 +0300

    Update matches.js

--- Commands/Random/matches.js
+++ Commands/Random/matches.js
@@ -1,5 +1,3 @@
-const moment = require('moment-timezone');
-
 module.exports = async (context) => {
     const { client, m, text, fetchJson } = context;
 
@@ -7,41 +5,30 @@ module.exports = async (context) => {
         let pl, laliga, bundesliga, serieA, ligue1;
 
         const plData = await fetchJson('https://api.dreaded.site/api/matches/PL');
-        pl = formatMatches('Premier League', plData.data);
+        pl = plData.data;
 
         const laligaData = await fetchJson('https://api.dreaded.site/api/matches/PD');
-        laliga = formatMatches('La Liga', laligaData.data);
+        laliga = laligaData.data;
 
         const bundesligaData = await fetchJson('https://api.dreaded.site/api/matches/BL1');
-        bundesliga = formatMatches('Bundesliga', bundesligaData.data);
+        bundesliga = bundesligaData.data;
 
         const serieAData = await fetchJson('https://api.dreaded.site/api/matches/SA');
-        serieA = formatMatches('Serie A', serieAData.data);
+        serieA = serieAData.data;
 
         const ligue1Data = await fetchJson('https://api.dreaded.site/api/matches/FR');
-        ligue1 = formatMatches('Ligue 1', ligue1Data.data);
+        ligue1 = ligue1Data.data;
 
         let message = `Today Football Matches ⚽\n\n`;
 
-        message += pl ? `${pl}\n` : "Premier League: No matches scheduled\n";
-        message += laliga ? `${laliga}\n` : "La Liga: No matches scheduled\n";
-        message += bundesliga ? `${bundesliga}\n` : "Bundesliga: No matches scheduled\n";
-        message += serieA ? `${serieA}\n` : "Serie A: No matches scheduled\n";
-        message += ligue1 ? `${ligue1}\n` : "Ligue 1: No matches scheduled\n";
+        message += pl ? `Premier League:\n${pl}\n` : "Premier League: No matches scheduled\n";
+        message += laliga ? `La Liga:\n${laliga}\n` : "La Liga: No matches scheduled\n";
+        message += bundesliga ? `Bundesliga:\n${bundesliga}\n` : "Bundesliga: No matches scheduled\n";
+        message += serieA ? `Serie A:\n${serieA}\n` : "Serie A: No matches scheduled\n";
+        message += ligue1 ? `Ligue 1:\n${ligue1}\n` : "Ligue 1: No matches scheduled\n";
 
         await m.reply(message);
     } catch (error) {
         m.reply('Something went wrong. Unable to fetch matches.');
     }
 };
-
-function formatMatches(leagueName, matchesData) {
-    if (matchesData === `No matches scheduled for ${leagueName} today.`) {
-        return `${leagueName}: No matches scheduled`;
-    }
-
-    const kenyaTime = moment(matchesData.match(/Date: (.*)/)[1]).tz('Africa/Nairobi').format('YYYY-MM-DD HH:mm:ss');
-    const matchDetails = `${matchesData.split(" - Date:")[0]} - Date: ${kenyaTime}`;
-
-    return `${leagueName}:\n${matchDetails}`;
-}
