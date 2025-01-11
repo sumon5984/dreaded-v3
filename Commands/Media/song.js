a9abc2a474414ab6faaafe9dc7eab08f10979b69
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-01-11 09:06:00 +0300

    Update song.js

--- Commands/Media/song.js
+++ Commands/Media/song.js
@@ -4,25 +4,32 @@ module.exports = async (context) => {
 
     try {
         if (!text) return m.reply("What song do you want to download?");
-        
+
         const { videos } = await yts(text);
         if (!videos || videos.length === 0) return m.reply("No songs found!");
 
         const urlYt = videos[0].url;
-        let data = await fetchJson(`https://api.dreaded.site/api/ytdl2/audio?url=${urlYt}`);
+        let data = await fetchJson(`https://api.dreaded.site/api/ytdl/audio?url=${urlYt}`);
 
-        if (!data || !data.result || !data.result.downloadUrl) {
+        if (!data || !data.result || !data.result.download || !data.result.download.url) {
             return m.reply("Failed to fetch audio from the API.");
         }
 
-        const { title: name, downloadUrl: audio } = data.result;
-        await m.reply(`_Downloading ${name}_`);
+        const {
+            metadata: { title, thumbnail, duration, author },
+            download: { url: audioUrl, quality, filename },
+        } = data.result;
+
+        
+
+        await m.reply(`_Downloading ${title}_`);
+
         await client.sendMessage(
             m.chat,
             {
-                audio: { url: audio },
+                audio: { url: audioUrl },
                 mimetype: "audio/mpeg",
-                fileName: `${name}.mp3`,
+                fileName: filename,
             },
             { quoted: m }
         );
