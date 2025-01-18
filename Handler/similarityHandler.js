f674a00379ef7b3aefc69493276750c3e24c74b1
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-01-18 09:14:01 +0300

    Update Handler

---
+++ Handler/similarityHandler.js
@@ -0,0 +1,53 @@
+const similarity = require('similarity');
+const path = require('path');
+const fs = require('fs');
+
+const cmdsDir = path.join(__dirname, '..', 'Commands');
+
+function findAllCommandFiles(dir) {
+    let commandFiles = [];
+    let totalCommands = 0;
+
+    function findFiles(directory) {
+        const files = fs.readdirSync(directory);
+
+        for (const file of files) {
+            const filePath = path.join(directory, file);
+            const stat = fs.statSync(filePath);
+
+            if (stat.isDirectory()) {
+                findFiles(filePath);
+            } else if (file.endsWith('.js')) {
+                commandFiles.push(filePath);
+                totalCommands++;
+            }
+        }
+    }
+
+    findFiles(dir);
+    return { commandFiles, totalCommands };
+}
+
+const { commandFiles } = findAllCommandFiles(cmdsDir);
+
+const getCommands = () => {
+    return commandFiles.map(file => path.basename(file, '.js'));
+};
+
+const findClosestCommand = (inputCommand, threshold = 0.6) => {
+    const availableCommands = getCommands();
+    let closestCommand = '';
+    let maxSimilarity = 0;
+
+    availableCommands.forEach(command => {
+        const similarityScore = similarity(inputCommand.toLowerCase(), command.toLowerCase());
+        if (similarityScore > maxSimilarity) {
+            maxSimilarity = similarityScore;
+            closestCommand = command;
+        }
+    });
+
+    return maxSimilarity >= threshold ? closestCommand : null;
+};
+
+module.exports = { findClosestCommand };
