25d49f83abb7a5a336b49252fec34a57c5bf1db0
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-01-18 16:23:06 +0300

    Update vision.js

--- Commands/AI/vision.js
+++ Commands/AI/vision.js
@@ -1,29 +1,43 @@
 const axios = require('axios');
 
 module.exports = async (context) => {
-    const { client, m, text, botname } = context;
+    const { client, mime, m, text, botname } = context;
 
-    if (!m.quoted) return m.reply("quote an image or pdf for analysis");
-    if (!text) return m.reply("Provide some instruction");
+    if (m.quoted && text) {
+        const buffer = await m.quoted.download();
+        
 
-    const query = text;
-    const buffer = await m.quoted.download();
-    const base64String = buffer.toString('base64');
+        if (!/image|pdf/.test(mime)) return m.reply("That's neither an image nor a PDF, quote a PDF document or an image with instructions.");
 
-    try {
-        const response = await axios.post('https://api.dreaded.site/api/gemini-analyze', {
-            query: query,
-            imageBuffer: base64String
-        }, {
-            headers: {
-                'Content-Type': 'application/json'  
-            }
-        });
+        const query = text;
+        const base64String = buffer.toString('base64');
 
-        console.log(response.data);
-        await m.reply(response.data.result);
-    } catch (error) {
-        console.error("Error in sending request:", error);
+        await m.reply(`A moment, dreaded is analyzing the contents of the ${mime.includes("pdf") ? "PDF document" : "image"} you provided...`);
+
+        try {
+            const response = await axios.post('https://api.dreaded.site/api/gemini-analyze', {
+                query: query,
+                imageBuffer: base64String
+            }, {
+                headers: {
+                    'Content-Type': 'application/json'  
+                }
+            });
+
+            console.log(response.data);
+            await m.reply(response.data.result);
+        } catch (error) {
+            const errorMessage = error.message || 'An unknown error occurred.';
+            const maxErrorLength = 200;
+            const replyMessage = errorMessage.length > maxErrorLength
+                ? errorMessage.substring(0, maxErrorLength) + '...'
+                : errorMessage;
+
+            console.error("Error in sending request:", error);
+            await m.reply(replyMessage);
+        }
+    } else {
+        m.reply("Quote a PDF or image with instructions for bot to analyse.");
     }
 }
 
