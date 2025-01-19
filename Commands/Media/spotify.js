82216238e420483f7c70ee7ee99ccd34620dbb96
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-01-19 15:23:22 +0300

    Update Media

---
+++ Commands/Media/spotify.js
@@ -0,0 +1,54 @@
+module.exports = async (context) => {
+    const { client, m, text, fetchJson } = context;
+
+        if (!text) return m.reply("What song do you want to download?");
+
+
+try {
+
+
+        let data = await fetchJson(`https://api.dreaded.site/api/spotifydl?title=${text}`);
+
+if (data.success) {
+
+await m.reply("Sending song in audio and document formats...");
+
+const audio = data.result.downloadLink;
+
+const filename = data.result.title
+
+        await client.sendMessage(
+            m.chat,
+            {
+                document: { url: audio },
+                mimetype: "audio/mpeg",
+                fileName: `${filename}.mp3`,
+            },
+            { quoted: m }
+        );
+
+await client.sendMessage(
+            m.chat,
+            {
+                audio: { url: audio },
+                mimetype: "audio/mpeg",
+                fileName: `${filename}.mp3`,
+            },
+            { quoted: m }
+        );
+
+
+} else {
+
+await m.reply("Failed to get a valid response from API endpoint");
+
+}
+
+} catch (error) {
+
+m.reply("An error occured\n\n" + error)
+
+}
+
+}
+
