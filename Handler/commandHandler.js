62bacb0be31ee980b119d063dc2a4a16b8db8c51
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2025-01-28 22:13:26 +0300

    Update commandHandler.js

--- Handler/commandHandler.js
+++ Handler/commandHandler.js
@@ -29,11 +29,64 @@ function findAllCommandFiles(dir) {
 
 const { commandFiles, totalCommands } = findAllCommandFiles(cmdsDir);
 
+
 const commands = {};
-commandFiles.forEach(file => {
+const aliases = {
+    speed: "ping", 
+kick: "remove",
+mute: "close", 
+unmute: "open", 
+    latency: "ping", 
+    runtime: "uptime",
+admin: "oadmin", 
+groups: "botgc",
+bc: "broadcast", 
+pp: "fullpp",  
+kickall: "kill",
+kickall2: "kill2", 
+exec: "shell", 
+leave: "leavegc",
+left: "leavegc",
+join: "joingc",
+gh: "github",
+ss: "screenshot", 
+ssweb: "ssweb",
+help: "menu",
+commands: "menu",
+list: "menu",
+repo: "script",
+sc: "script",
+linkgc: "link",
+gclink: "link",
+grouplink: "link",
+linkgroup: "link",
+mention: "tagall", 
+reset: "revoke",
+mute: "close", 
+app: "apk",
+fb: "fbdl", 
+facebook: "fbdl",
+url: "upload", 
+tourl: "upload", 
+yta: "ytmp3",
+ytv: "ytmp4", 
+mf: "mediafire",
+emojimix: "emix",  
+enc: "encrypt",
+req: "requests",
+approve: "approve-all",
+reject: "reject-all",  
+s: "sticker",                        
+    up: "uptime" 
+};
+
+
+commandFiles.forEach((file) => {
     const commandName = path.basename(file, '.js');
     const commandModule = require(file);
+
+
     commands[commandName] = commandModule;
 });
 
-module.exports = { commands, totalCommands };
+module.exports = { commands, aliases, totalCommands };
